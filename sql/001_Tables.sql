dcff9ab5d368244b6b2e21e2e204be46c653200e
Joaquin Tarazona <joaquin.tarazona@ec.pe>
2015-05-22 18:12:40 -0500

    Developer Init

--- sql/001_Tables.sql
+++ sql/001_Tables.sql
@@ -16,8 +16,50 @@ CREATE TABLE `auth_usuario` (
    fecha_creacion DATETIME NULL,
    us_estado int(11) NULL DEFAULT 1,
   PRIMARY KEY (`us_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- users data with password = md5('admin')
 INSERT INTO `auth_usuario` (`us_usuario`, `us_password`,us_email,fecha_creacion) VALUES
 ('admin', md5('tarazona'),'jmike410@gmail.com',now());
+
+CREATE TABLE `auth_rol` (
+  `rol_id` INT NOT NULL AUTO_INCREMENT,
+  `rol_dec` VARCHAR(128) NULL,
+  `rol_estado` INT(11) NULL,
+  `rol_rol_id` INT NOT NULL,
+  PRIMARY KEY (`rol_id`),
+  INDEX `fk_auth_rol_auth_rol_idx` (`rol_rol_id` ASC),
+  CONSTRAINT `fk_auth_rol_auth_rol`
+    FOREIGN KEY (`rol_rol_id`)
+    REFERENCES `auth_rol` (`rol_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `auth_recurso` (
+  `rec_id` INT NOT NULL AUTO_INCREMENT,
+  `rec_desc` VARCHAR(256) NULL,
+  `rec_uri` VARCHAR(256) NULL,
+  `rec_estado` INT(11) NULL,
+  PRIMARY KEY (`rec_id`))
+ENGINE = InnoDB;
+
+CREATE TABLE `auth_rol_recurso` (
+  `auth_rol_rol_id` INT NOT NULL,
+  `auth_recurso_rec_id` INT NOT NULL,
+  `rolrec_desc` VARCHAR(45) NULL COMMENT 'Si: allow\nNo: Deny',
+  `rolrec_estado` VARCHAR(45) NULL,
+  PRIMARY KEY (`auth_rol_rol_id`, `auth_recurso_rec_id`),
+  INDEX `fk_auth_rol_has_auth_recurso_auth_recurso1_idx` (`auth_recurso_rec_id` ASC),
+  INDEX `fk_auth_rol_has_auth_recurso_auth_rol1_idx` (`auth_rol_rol_id` ASC),
+  CONSTRAINT `fk_auth_rol_has_auth_recurso_auth_rol1`
+    FOREIGN KEY (`auth_rol_rol_id`)
+    REFERENCES `auth_rol` (`rol_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_auth_rol_has_auth_recurso_auth_recurso1`
+    FOREIGN KEY (`auth_recurso_rec_id`)
+    REFERENCES `auth_recurso` (`rec_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
